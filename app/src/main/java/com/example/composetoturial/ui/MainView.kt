6ab1267c20e7496b98ad6e417b62d1273fae181c
Milad Faridnia <MiladFaridnia@users.noreply.github.com>
2023-01-23 19:21:43 +0330

    Feat: Button, Spacer, Modifier, Text ...

--- app/src/main/java/com/example/composetoturial/ui/MainView.kt
+++ app/src/main/java/com/example/composetoturial/ui/MainView.kt
@@ -1,29 +1,50 @@
 package com.example.composetoturial.ui
 
+import android.widget.Toast
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material.Button
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun MainView() {
-    Surface(
+
+    val context = LocalContext.current
+
+    Column(
         modifier = Modifier.fillMaxSize(),
-        color = MaterialTheme.colors.background
-    ) {
-        Greeting("Milad")
+        horizontalAlignment = Alignment.CenterHorizontally,
+
+        ) {
+        Spacer(modifier = Modifier
+            .height(32.dp)
+            .padding(16.dp))
+        Button(onClick = {
+            Toast.makeText(context , "Button Clicked", Toast.LENGTH_LONG).show()
+        }) {
+            Text(
+                text = "Sample Button",
+                color = Color.Green
+            )
+        }
+        ShowText("Milad")
+        ShowText("Milad")
+        ShowText("Milad")
     }
 }
 
 @Composable
-fun Greeting(name: String) {
+fun ShowText(name: String) {
     Text(text = "Hello $name!")
 }
 
-@Preview(showBackground = true , showSystemUi = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun DefaultPreview() {
     MainView()
