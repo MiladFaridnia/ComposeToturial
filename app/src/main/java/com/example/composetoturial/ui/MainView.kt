c71b944320045ef5a20ef8920f7a485427262928
Milad Faridnia <MiladFaridnia@users.noreply.github.com>
2023-04-08 17:40:27 +0430

    Feat: Remember and Remember Savable

--- app/src/main/java/com/example/composetoturial/ui/MainView.kt
+++ app/src/main/java/com/example/composetoturial/ui/MainView.kt
@@ -3,9 +3,10 @@ package com.example.composetoturial.ui
 import android.widget.Toast
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.material.Button
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -20,28 +21,62 @@ fun MainView() {
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Bottom
+    ) {
 
-        ) {
-        Spacer(modifier = Modifier
-            .height(32.dp)
-            .padding(16.dp))
+        var buttonTitle by remember {
+            mutableStateOf("Milad Agha Milad")
+        }
+
+//        var buttonTitle by rememberSaveable {
+//            mutableStateOf("Milad Agha Milad")
+//        }
+
+        Spacer(
+            modifier = Modifier
+                .height(32.dp)
+                .padding(16.dp)
+        )
+
         Button(onClick = {
-            Toast.makeText(context , "Button Clicked", Toast.LENGTH_LONG).show()
-        }) {
+
+            buttonTitle = if (buttonTitle == "Agha Bardia is the best") {
+                "Bardia is the great"
+            } else {
+                "Agha Bardia is the best"
+            }
+
+            Toast.makeText(context, "Button Clicked", Toast.LENGTH_LONG).show()
+        })
+        {
             Text(
-                text = "Sample Button",
+                text = buttonTitle,
                 color = Color.Green
             )
         }
-        ShowText("Milad")
-        ShowText("Milad")
-        ShowText("Milad")
+
+        Spacer(
+            modifier = Modifier
+                .height(40.dp)
+                .padding(16.dp)
+        )
+
+        ShowText(buttonTitle)
+        ShowText("Hello Milad")
+        ShowText("Hi Agha Bardia")
+
+        Spacer(
+            modifier = Modifier
+                .height(80.dp)
+                .padding(16.dp)
+        )
+
     }
 }
 
 @Composable
 fun ShowText(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "$name!")
 }
 
 @Preview(showBackground = true, showSystemUi = true)
